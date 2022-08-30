0d9633a5ab915e1e70883d584b58e8658b1e9e25
Michael Braun <m.braun92@gmail.com>
2022-08-30 09:09:59 +0200

    feature: allow up to 11 booth urls to be crawled

--- src/booth.ts
+++ src/booth.ts
@@ -26,36 +26,52 @@ export const client = axios.default.create({
   httpsAgent
 });
 
+const boothUrls = [
+  process.env.BOOTH_FETCH_URL,
+  process.env.BOOTH_FETCH_URL_0,
+  process.env.BOOTH_FETCH_URL_1,
+  process.env.BOOTH_FETCH_URL_2,
+  process.env.BOOTH_FETCH_URL_3,
+  process.env.BOOTH_FETCH_URL_4,
+  process.env.BOOTH_FETCH_URL_5,
+  process.env.BOOTH_FETCH_URL_6,
+  process.env.BOOTH_FETCH_URL_7,
+  process.env.BOOTH_FETCH_URL_8,
+  process.env.BOOTH_FETCH_URL_9
+].filter(Boolean);
+
 export async function getNewestItems(): Promise<BoothItem[]> {
   let items = [] as BoothItem[];
 
-  let response = await client.request<string>({
-    url: process.env.BOOTH_FETCH_URL
-  });
-
-  let $ = cheerio.load(response.data);
-
-  $('.item-card').each((_index, el) => {
-    let thumbnailImages = [] as string[];
-
-    $('.item-card__thumbnail-image', el).each((_subIndex, subEl) => {
-      let imageUrl = $(subEl).attr('data-original');
-      if (imageUrl === void 0) {
-        return;
-      }
-      thumbnailImages.push(imageUrl);
+  for (const url of boothUrls) {
+    let response = await client.request<string>({
+      url: process.env.BOOTH_FETCH_URL
     });
-
-    items.push({
-      id: $(el).data('productId') ?? '',
-      title: $('.item-card__title', el).text(),
-      price: $('.price', el).text(),
-      shopName: $('.item-card__shop-name', el).text(),
-      shopUrl: $('.item-card__shop-name-anchor', el).attr('href') ?? '',
-      shopImageUrl: $('.user-avatar', el).attr('src') ?? '',
-      thumbnailImageUrls: thumbnailImages
-    } as BoothItem);
-  });
+  
+    let $ = cheerio.load(response.data);
+  
+    $('.item-card').each((_index, el) => {
+      let thumbnailImages = [] as string[];
+  
+      $('.item-card__thumbnail-image', el).each((_subIndex, subEl) => {
+        let imageUrl = $(subEl).attr('data-original');
+        if (imageUrl === void 0) {
+          return;
+        }
+        thumbnailImages.push(imageUrl);
+      });
+  
+      items.push({
+        id: $(el).data('productId') ?? '',
+        title: $('.item-card__title', el).text(),
+        price: $('.price', el).text(),
+        shopName: $('.item-card__shop-name', el).text(),
+        shopUrl: $('.item-card__shop-name-anchor', el).attr('href') ?? '',
+        shopImageUrl: $('.user-avatar', el).attr('src') ?? '',
+        thumbnailImageUrls: thumbnailImages
+      } as BoothItem);
+    });
+  }
 
   return items;
 }
